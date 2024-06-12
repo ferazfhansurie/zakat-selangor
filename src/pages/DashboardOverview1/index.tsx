183bcd5fa647807f0bbb827412913c39d9fc8d0b
faieqlano <ifaieq12@gmail.com>
2024-06-12 18:05:43 +0800

    Dashboard ui and charts

--- src/pages/DashboardOverview1/index.tsx
+++ src/pages/DashboardOverview1/index.tsx
@@ -20,11 +20,14 @@ import axios from 'axios';
 import { getFirebaseToken, messaging } from "../../firebaseconfig";
 import { getAuth } from "firebase/auth";
 import { initializeApp } from "firebase/app";
-import { DocumentReference, getDoc, onSnapshot } from 'firebase/firestore';
+import { DocumentData, DocumentReference, getDoc, getDocs, onSnapshot } from 'firebase/firestore';
 import { getFirestore, collection, doc, setDoc, DocumentSnapshot } from 'firebase/firestore';
 import { onMessage } from "firebase/messaging";
 import { useNavigate } from "react-router-dom"; // Add this import
 import LoadingIcon from "@/components/Base/LoadingIcon";
+import { Bar, Doughnut } from "react-chartjs-2";
+import { BarChart } from "lucide-react";
+import { useContacts } from "@/contact";
 
 const firebaseConfig = {
   apiKey: "AIzaSyCc0oSHlqlX7fLeqqonODsOIC3XA8NI7hc",
@@ -43,21 +46,59 @@ const auth = getAuth(app);
 
 let companyId = "";
 let total_contacts = 0;
-let closed = 0;
-let unclosed = 0;
-let num_replies =0;
 let role = 2;
 function Main() {
+
+  interface Contact {
+    additionalEmails: string[];
+    address1: string | null;
+    assignedTo: string | null;
+    businessId: string | null;
+    city: string | null;
+    companyName: string | null;
+    contactName: string;
+    country: string;
+    customFields: any[];
+    dateAdded: string;
+    dateOfBirth: string | null;
+    dateUpdated: string;
+    dnd: boolean;
+    dndSettings: any;
+    email: string | null;
+    firstName: string;
+    followers: string[];
+    id: string;
+    lastName: string;
+    locationId: string;
+    phone: string | null;
+    postalCode: string | null;
+    source: string | null;
+    state: string | null;
+    tags: string[];
+    type: string;
+    website: string | null;
+  
+  }
+  
   const [salesReportFilter, setSalesReportFilter] = useState<string>();
   const importantNotesRef = useRef<TinySliderElement>();
-  const [data, setData] = useState<any>(null);
+  const [, setData] = useState<any>(null);
   const [loading, setLoading] = useState<boolean>(false);
   const [error, setError] = useState<string | null>(null);
   const [notifications, setNotifications] = useState<any[]>([]);
+  const { contacts: initialContacts} = useContacts();
+  const [contacts, setContacts] = useState<Contact[]>(initialContacts);
+  const [totalContacts, setTotalContacts] = useState(initialContacts.length);
   const [closed, setClosed] = useState(0);
   const [unclosed, setUnclosed] = useState(0);
-  const [num_replies, setReplies] = useState(0);
+  const [numReplies, setReplies] = useState(0);
+  const [abandoned, setAbandoned] = useState(0);
   const [searchQuery, setSearchQuery] = useState("");
+
+  useEffect(() => {
+    fetchCompanyData();
+  }, []);
+  
 
   const handleSearchChange = (e: { target: { value: string; }; }) => {
     setSearchQuery(e.target.value.toLowerCase());
@@ -176,6 +217,14 @@ function Main() {
       }
       
       await requestPermission(userEmail);
+
+      // Fetch notifications from the notifications subcollection
+    const notificationsRef = collection(firestore, 'user', userEmail, 'notifications');
+    const notificationsSnapshot = await getDocs(notificationsRef);
+    const notifications = notificationsSnapshot.docs.map((doc: { data: () => DocumentData; }) => doc.data());
+    console.log(notifications);
+
+    setReplies(notifications.length);
   
       await searchOpportunities( data.ghl_accessToken,data.ghl_location);
       //await searchContacts(data.ghl_accessToken, data.ghl_location);
@@ -220,51 +269,66 @@ function Main() {
     }
   }
 
-  async function searchOpportunities(ghlToken: any, locationId: any) {
-    setLoading(true);
-    setError(null);
-  console.log(locationId);
-    let allOpportunities: any[] = [];
-    let page = 1;
-    let totalFetched = 0;
-    let hasMore = true;
+  const searchOpportunities = async (ghlToken: any, locationId: any) => {
 
-    while (hasMore) {
-      const response = await axios.get('https://services.leadconnectorhq.com/opportunities/search', {
-        headers: {
-          Authorization: `Bearer ${ghlToken}`,
-          Version: '2021-07-28',
-          Accept: 'application/json'
-        },
-        params: {
-          location_id: locationId,
-          limit: 100,
-          page: page
+    try {
+      let allOpportunities: any[] = [];
+      let page = 1;
+      let totalFetched = 0;
+      let hasMore = true;
+
+      while (hasMore) {
+        const response = await axios.get('https://services.leadconnectorhq.com/opportunities/search', {
+          headers: {
+            Authorization: `Bearer ${ghlToken}`,
+            Version: '2021-07-28',
+            Accept: 'application/json'
+          },
+          params: {
+            location_id: locationId,
+            limit: 100,
+            page: page
+          }
+        });
+
+        const opportunities = response.data.opportunities;
+        totalFetched = opportunities.length;
+        allOpportunities = [...opportunities, ...opportunities];
+
+        if (totalFetched < 100) {
+          hasMore = false;
+        } else {
+          page++;
         }
-      });
-console.log(response.data);
-      const opportunities = response.data.opportunities;
-      totalFetched = opportunities.length;
-      allOpportunities = [...allOpportunities, ...opportunities];
-
-      // Check if there are more opportunities to fetch
-      if (totalFetched < 100) {
-        hasMore = false;
-      } else {
-        page++;
       }
+
+      const closedCount = allOpportunities.filter(opportunity => opportunity.status === 'won').length;
+      const unclosedCount = allOpportunities.filter(opportunity => opportunity.status === 'open').length;
+      const abandonedCount = allOpportunities.filter(opportunity => opportunity.status === 'abandoned').length;
+
+   
+      setClosed(closedCount);
+      setUnclosed(unclosedCount);
+      setAbandoned(abandonedCount)
+    } catch (error) {
+      console.error('Error searching opportunities:', error);
     }
+  };
 
-    
+  const data = {
+    labels: ['Total Contacts', 'Number Of Replies', 'Leads', 'Closed Leads', 'Abandoned'],
+    datasets: [
+      {
+        data: [totalContacts, numReplies, unclosed, closed, abandoned],
+        backgroundColor: ['#4338CA', '#818CF8', '#2563EB', '#93C5FD', '#b375bd'],
+        hoverBackgroundColor: ['#312E81', '#4F46E5', '#1E40AF', '#3B82F6', '#b375bd']
+      }
+    ]
+  };
 
-   const total_contacts = allOpportunities.length;
-    const closedCount = allOpportunities.filter(opportunity => opportunity.status === 'won').length;
-    const unclosedCount = allOpportunities.filter(opportunity => opportunity.status === 'open').length;
-    setTotalContacts(total_contacts);
-    setClosed(closedCount);
-    setUnclosed(unclosedCount);
-    setLoading(false);
-  }
+  const options = {
+    cutout: '85%' // Adjust this value to decrease or increase the doughnut thickness
+  };
 
   const prevImportantNotes = () => {
     importantNotesRef.current?.tns.goTo("prev");
@@ -273,9 +337,6 @@ console.log(response.data);
   const nextImportantNotes = () => {
     importantNotesRef.current?.tns.goTo("next");
   };
-
-  const [totalContacts, setTotalContacts] = useState(0);
-
 
   async function fetchOpportunities(accessToken: any, locationId: any) {
     try {
@@ -330,9 +391,7 @@ console.log(response.data);
  
 
       // Assuming refreshAccessToken is defined elsewhere
-console.log(companyData);
-
-      await searchContacts(companyData.ghl_accessToken,companyData.ghl_location);
+      console.log(companyData);
 
 
     } catch (error) {
@@ -353,124 +412,113 @@ console.log(companyData);
               </h2>
              
             </div>
-            <div className="grid grid-cols-12 gap-6 mt-5">
-              <div className="col-span-12 sm:col-span-6 xl:col-span-3 intro-y">
-                <div
-                  className={clsx([
-                    "relative zoom-in",
-                    "before:box before:absolute before:inset-x-3 before:mt-3 before:h-full before:bg-slate-50 before:content-['']",
-                  ])}
-                >
-                  <div className="p-5 box">
-                    <div className="flex">
+            <div className="grid grid-cols-12 gap-4 mt-5">
+              <div className="item-center col-span-2 sm:col-span-2 xl:col-span-2 intro-y">
+                <div>
+                  <div className="flex p-4 box text-right">
+                    <span className="">
                       <Lucide
-                        icon="ShoppingCart"
-                        className="w-[28px] h-[28px] text-primary"
+                        icon="Contact"
+                        className="w-[56px] h-[56px] mb-0 text-blue-800 hover:text-blue-900"
                       />
-                      <div className="ml-auto"></div>
-                    </div>
-                    <div className="mt-6 text-3xl font-medium leading-8">
-                     
+                    </span>
+                    <div className="pl-5 text-5xl font-medium">
                       {!loading && totalContacts}
                       {loading && (
-         <div className="flex flex-col items-center justify-end col-span-6 sm:col-span-3 xl:col-span-2">
-         <LoadingIcon icon="spinning-circles" className="w-8 h-8" />
-   
-       </div>
-      )}
-                    </div>
-                    <div className="mt-1 text-base text-slate-500">
+                        <div className="flex flex-col items-center justify-end col-span-6 sm:col-span-3 xl:col-span-2">
+                        <LoadingIcon icon="spinning-circles" className="w-8 h-8" />
+                      </div>
+                      )}
+                      <div className="mt-1 text-sm text-slate-400">
                       Total Contacts
                     </div>
+                    </div>
                   </div>
                 </div>
               </div>
-              <div className="col-span-12 sm:col-span-6 xl:col-span-3 intro-y">
-                <div
-                  className={clsx([
-                    "relative zoom-in",
-                    "before:box before:absolute before:inset-x-3 before:mt-3 before:h-full before:bg-slate-50 before:content-['']",
-                  ])}
-                >
-                  <div className="p-5 box">
-                    <div className="flex">
+              <div className="item-center col-span-2 sm:col-span-2 xl:col-span-2 intro-y">
+                <div>
+                  <div className="flex p-4 box text-right">
+                    <span className="">
                       <Lucide
-                        icon="CreditCard"
-                        className="w-[28px] h-[28px] text-pending"
+                        icon="MessageCircleReply"
+                        className="w-[56px] h-[56px] text-blue-600 hover:text-blue-700"
                       />
-                      <div className="ml-auto"></div>
-                    </div>
-                    <div className="mt-6 text-3xl font-medium leading-8">
-                    {!loading && num_replies}
+                    </span>
+                    <div className="pl-5 text-5xl font-medium">
+                    {!loading && numReplies}
                       {loading && (
-         <div className="flex flex-col items-center justify-end col-span-6 sm:col-span-3 xl:col-span-2">
-         <LoadingIcon icon="spinning-circles" className="w-8 h-8" />
-        
-       </div>
-      )}
-                    </div>
-                    <div className="mt-1 text-base text-slate-500">
-                      Number Of Replies
+                        <div className="flex flex-col items-center justify-end col-span-6 sm:col-span-3 xl:col-span-2">
+                        <LoadingIcon icon="spinning-circles" className="w-8 h-8" />
+                        
+                      </div>)}
+                      <div className="mt-1 text-sm text-slate-400">
+                      Number Replies
+                      </div>
                     </div>
                   </div>
                 </div>
               </div>
-              <div className="col-span-12 sm:col-span-6 xl:col-span-3 intro-y">
-                <div
-                  className={clsx([
-                    "relative zoom-in",
-                    "before:box before:absolute before:inset-x-3 before:mt-3 before:h-full before:bg-slate-50 before:content-['']",
-                  ])}
-                >
-                  <div className="p-5 box">
-                    <div className="flex">
+              <div className="item-center col-span-2 sm:col-span-2 xl:col-span-2 intro-y">
+                <div>
+                  <div className="flex p-4 box text-right">
+                    <span className="">
                       <Lucide
-                        icon="Monitor"
-                        className="w-[28px] h-[28px] text-warning"
+                        icon="Mail"
+                        className="w-[56px] h-[56px] text-blue-400 hover:text-blue-500"
                       />
-                      <div className="ml-auto"></div>
-                    </div>
-                    <div className="mt-6 text-3xl font-medium leading-8">
+                    </span>
+                    <div className="pl-5 text-5xl font-medium">
                     {!loading && unclosed}
                       {loading && (
-         <div className="flex flex-col items-center justify-end col-span-6 sm:col-span-3 xl:col-span-2">
-         <LoadingIcon icon="spinning-circles" className="w-8 h-8" />
-     
-       </div>
-      )}
-                    </div>
-                    <div className="mt-1 text-base text-slate-500">
-                      Open
+                        <div className="flex flex-col items-center justify-end col-span-6 sm:col-span-3 xl:col-span-2">
+                        <LoadingIcon icon="spinning-circles" className="w-8 h-8" />
+                    
+                      </div>)}
+                      <div className="mt-1 text-sm text-slate-400">
+                      Total Leads
+                      </div>
                     </div>
                   </div>
                 </div>
               </div>
-              <div className="col-span-12 sm:col-span-6 xl:col-span-3 intro-y">
-                <div
-                  className={clsx([
-                    "relative zoom-in",
-                    "before:box before:absolute before:inset-x-3 before:mt-3 before:h-full before:bg-slate-50 before:content-['']",
-                  ])}
-                >
-                  <div className="p-5 box">
-                    <div className="flex">
+              <div className="item-center col-span-2 sm:col-span-2 xl:col-span-2 intro-y">
+                <div>
+                  <div className="flex p-4 box text-right">
+                    <span className="">
                       <Lucide
-                        icon="User"
-                        className="w-[28px] h-[28px] text-success"
+                        icon="Check"
+                        className="w-[56px] h-[56px] text-blue-200 hover:text-blue-300"
                       />
-                      <div className="ml-auto"></div>
-                    </div>
-                    <div className="mt-6 text-3xl font-medium leading-8">
+                    </span>
+                    <div className="pl-5 text-5xl font-medium">
                     {!loading && closed}
                       {loading && (
-        <div className="flex flex-col items-center justify-end col-span-6 sm:col-span-3 xl:col-span-2">
-        <LoadingIcon icon="spinning-circles" className="w-8 h-8" />
-     
-      </div>
-      )}
+                        <div className="flex flex-col items-center justify-end col-span-6 sm:col-span-3 xl:col-span-2">
+                        <LoadingIcon icon="spinning-circles" className="w-8 h-8" />
+                      </div>)}
+                      <div className="mt-1 text-sm text-slate-400">
+                      Closed Leads
+                      </div>
                     </div>
-                    <div className="mt-1 text-base text-slate-500">
-                      Closed
+                  </div>
+                </div>
+              </div>
+              <br></br>
+              <div className="col-span-4 sm:col-span-4 xl:col-span-4 intro-y">
+                <div className="relative">
+                  <div className="p-2 box">
+                    <div className="flex justify-center">
+                      <Doughnut data={data} options={options}/>
+                    </div>
+                  </div>
+                </div>
+              </div>
+              <div className="col-span-4 sm:col-span-4 xl:col-span-4 intro-y">
+                <div className="relative">
+                  <div className="p-2 box">
+                    <div className="flex justify-center">
+                      <Bar data={data}/>
                     </div>
                   </div>
                 </div>
